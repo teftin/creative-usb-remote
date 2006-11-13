a3c2f6552cbfac3691c606879b1a070dcc156e7d
ecto <ecto>
2006-11-13 23:52:01 +0000

    wip

--- hid_read.cc
+++ hid_read.cc
@@ -1,4 +1,4 @@
-//
+// $Id: hid_read.cc,v 1.3 2006/11/13 23:52:01 ecto Exp $
 
 #include <iostream>
 #include <string>
@@ -42,12 +42,29 @@ int main() {
 
     struct hiddev_usage_ref uref;
     while ( (nb = read(hid_s, &uref, sizeof(uref))) >= 0 ) {
+
+//      if ( uref.usage_index == 0 )
+//        cout <<"+key event" <<endl;
+//      else
+//        cout <<"+ [0x" <<setw(2) <<uref.usage_index <<"]: " <<(uref.value?"up":"down") <<endl;
+
       cout
         <<"+got uref [" <<nb <<"]:" <<endl
         <<"+ type[0x" <<uref.report_type <<"], id[0x" <<uref.report_id <<"]" <<endl
         <<"+ f_idx[0x" <<uref.field_index <<"]" <<endl
         <<"+ u_idx[0x" <<uref.usage_index <<"], u_code[0x" <<uref.usage_code <<"]" <<endl
         <<"+ value[0x" <<uref.value <<"]" <<endl <<endl;
+
+      if ( uref.usage_index ) {
+        if ( (uref.usage_code - uref.usage_index) != 0xfeffffff )
+          cout <<"PUK!" <<endl;
+
+      }else {
+
+        if ( uref.usage_index )
+          cout <<"PUK!" <<endl;
+
+      }
     }
 
     if ( nb < 0 )
@@ -68,3 +85,42 @@ int main() {
   return 0;
 
 }
+
+
+/*
+
+power      0x86 0x79
+1          0xd1 0x2e
+2          0xf1 0x0e
+3          0x09 0xf6
+4          0x51 0xae
+5          0x21 0xde
+6          0x1e 0xe1
+7          0x91 0x6e
+8          0x3e
+9          0xee 0x11
+0          0x01 0xfe
+cmss       0x8e 0x71
+mute       0x76 0x89
+rec        0xce 0x31
+vol-       0x8e 0x71
+vol+       0x31 0xce
+stop-eject 0xa1 0x5e
+play-pause 0x9e 0x61
+slow       0xbe 0x41
+prev       0xfe 0x01
+next       0x5e 0xa1
+step       0x7e 0x81
+eax        0x31 0xce
+options    0x41 0xbe
+display    0x6e 0x91
+return     0x71 0x8e
+start      0x11 0xee
+cancel     0x3e
+up         0xde 0x21
+left       0xe1 0xee
+ok         0x81 0x7e
+right      0xae 0x51
+down       0xbe 0x4e
+
+*/
