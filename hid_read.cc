3cab18dd7628799d1b355352931b57d49bb02bb4
ecto <ecto>
2006-11-13 23:04:08 +0000

    wip

--- hid_read.cc
+++ hid_read.cc
@@ -17,57 +17,54 @@ using namespace std;
 #include <linux/hiddev.h>
 
 int main() {
-    cout <<"***irread test***" <<endl;
+  cout <<"***irread test***" <<endl;
 
-    int hid_s = -1;
+  int hid_s = -1;
 
-    try {
-        cout <<"*start" <<endl;
-//        cout <<"+xmlrec initialized" <<endl;
+  try {
+    cout <<"*start" <<endl;
+    hid_s = open( "/dev/usb/hiddev0", O_RDONLY );
 
-//        ifstream hid("/dev/usb/hiddev0", ios_base::in | ios_base::binary );
-//
-//        if ( not hid.is_open() )
-//            throw string("cannot open hiddev");
+    if ( hid_s < 0 )
+      throw string("open(): ") + strerror(errno);
 
-        hid_s = open( "/dev/usb/hiddev0", O_RDONLY );
+    cout <<hex <<setfill('0');
 
-        if ( hid_s < 0 )
-            throw string("open(): ") + strerror(errno);
+    int flags;
+    flags = HIDDEV_FLAG_UREF | HIDDEV_FLAG_REPORT;
+    if ( ioctl(hid_s, HIDIOCSFLAG, &flags) )
+      throw string("ioctl(SET): ") + strerror(errno);
+    if ( ioctl(hid_s, HIDIOCGFLAG, &flags) < 0 )
+      throw string("ioctl(GET): ") + strerror(errno);
+    cout <<"*flags: 0x" <<flags <<endl;
+    
+    ssize_t nb;
 
-        cout <<hex <<setfill('0');
-
-        int flags;
-        if ( ioctl(hid_s, HIDIOCGFLAG, &flags) < 0 )
-            throw string("ioctl(GET): ") + strerror(errno);
-        cout <<"*flags: 0x" <<flags <<endl;
-//        flags |= HIDDEV_FLAG_UREF;
-//        if ( ioctl(hid_s, HIDIOCSFLAG, &flags) )
-//            throw string("ioctl(SET): ") + strerror(errno);
-//        cout <<"*flags: setting" <<endl;
-
-        struct hiddev_event event;
-        ssize_t nb;
-
-        while ( (nb = read(hid_s, &event, sizeof(event))) >= 0 ) {
-            cout <<"+got event [" <<nb <<"]: hid[0x" <<event.hid <<"], val[0x" <<event.value <<"]" <<endl; 
-
-        }
-        if ( nb < 0 )
-            throw string("read(): ") + strerror(errno);
-
-        close(hid_s);
-        cout <<"+finished normaly" <<endl;
-
-    }catch ( const string &ex ) {
-        cout <<"!got exception: " <<ex <<endl;
-        if ( hid_s >= 0 )
-          close(hid_s);
-
-        return 1;
-
+    struct hiddev_usage_ref uref;
+    while ( (nb = read(hid_s, &uref, sizeof(uref))) >= 0 ) {
+      cout
+        <<"+got uref [" <<nb <<"]:" <<endl
+        <<"+ type[0x" <<uref.report_type <<"], id[0x" <<uref.report_id <<"]" <<endl
+        <<"+ f_idx[0x" <<uref.field_index <<"]" <<endl
+        <<"+ u_idx[0x" <<uref.usage_index <<"], u_code[0x" <<uref.usage_code <<"]" <<endl
+        <<"+ value[0x" <<uref.value <<"]" <<endl <<endl;
     }
 
-    return 0;
+    if ( nb < 0 )
+      throw string("read(): ") + strerror(errno);
+
+    close(hid_s);
+    cout <<"+finished normaly" <<endl;
+
+  }catch ( const string &ex ) {
+    cout <<"!got exception: " <<ex <<endl;
+    if ( hid_s >= 0 )
+      close(hid_s);
+
+    return 1;
+
+  }
+
+  return 0;
 
 }
